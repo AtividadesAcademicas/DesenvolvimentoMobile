3dae739fa703a5a493232dd4b7566b8c065d8201
Pessinaaa <pedro.pessina99@gmail.com>
2023-08-19 10:26:39 -0300

    Atividade01 finalizada

--- Atividade01/app/src/main/java/com/example/atividade01/ActivityPrincipal.java
+++ Atividade01/app/src/main/java/com/example/atividade01/ActivityPrincipal.java
@@ -2,6 +2,9 @@ package com.example.atividade01;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,21 @@ public class ActivityPrincipal extends AppCompatActivity {
         Log.d("ActivityPrincipal", "onCreate executado");
         setContentView(R.layout.activity_principal);
         Log.d("ActivityPrincipal", "View carregada");
+
+        View view = findViewById(R.id.btSalvar);
+        EditText nome = (EditText) findViewById(R.id.InputNome);
+        EditText email = (EditText) findViewById(R.id.InputEmail);
+        EditText telefone = (EditText) findViewById(R.id.InputTelefone);
+
+        if (view instanceof Button) {
+            Button bt = (Button) view;
+            bt.setOnClickListener(v -> {
+                Log.d("ActivityPrincipal", "Botão Salvar clicado");
+                Log.d("ActivityPrincipal", "Nome: " + nome.getText().toString());
+                Log.d("ActivityPrincipal", "Email: " + email.getText().toString());
+                Log.d("ActivityPrincipal", "Telefone: " + telefone.getText().toString());
+            });
+        }
     }
 
     @Override
